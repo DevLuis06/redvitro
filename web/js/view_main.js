e3d185d3a99775890c306f25325b5a93bdc466bb
Yeltsin <Yeltsin@192.168.121.195>
2019-01-07 14:20:17 -0600

    update_new_colors_and_corrections

--- web/js/view_main.js
+++ web/js/view_main.js
@@ -2,15 +2,35 @@ function toggle(id) {
     var element = document.getElementById(id);
     var sobget = document.getElementsByClassName("ui-helper-hidden-accessible");
     
+    var element2 = document.getElementById("j_idt6:j_idt17:j_idt19:0");
+    
+    
+ alert(element2.value);
     for (var i = 0; i < 4; i++) {
         console.log(sobget[i].value);
-        if(element.style.display === 'block' && sobget[i].value === "asegurado") {
+        if(sobget[i].value === "asegurado"){
+           console.log(sobget[i].value+": 0"); 
+        }else{
+            
+        }
+       
+        
+       
+        
+       /* if(element.style.display === 'block' && sobget[i].value === "asegurado") {
             element.classList.remove("ui-state-disabled");
             element.classList.add("ui-state-default");
             element.style.display = "";
             alert("hecho");
-            
-        } else if(element.style.display === 'block' && sobget[i].value === "general"){
+            break;
+        }else{
+            /*element.classList.remove("ui-state-default");
+            element.classList.add("ui-state-disabled");
+            element.style.display = "block";*/
+        // --}
+        
+        
+        /*else if(element.style.display === 'block' && sobget[i].value === "general"){
             element.classList.remove("ui-state-default");
             element.classList.add("ui-state-disabled");
             element.style.display = "block";
