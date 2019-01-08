158d21d171f0d3dee5606354b251ca8e0e95cad3
Yeltsin <Yeltsin@192.168.121.195>
2019-01-08 15:13:02 -0600

    update_new_functions

--- web/js/view_main.js
+++ web/js/view_main.js
@@ -1,43 +1,35 @@
-function toggle(id) {
-    var element = document.getElementById(id);
-    var sobget = document.getElementsByClassName("ui-helper-hidden-accessible");
+function aseg_clk(){
+    var combo_ = document.getElementById('j_idt6:j_idt17:asegu');
     
-    var element2 = document.getElementById("j_idt6:j_idt17:j_idt19:0");
+    if(combo_.style.display === 'block'){
+        combo_.classList.remove("ui-state-disabled");
+        combo_.style.display = "";
+    }else{
+        combo_.classList.add("ui-state-disabled");
+        combo_.style.display = "block";
+    }
+}
+
+function gara_clk(){
+    var input_gara = document.getElementById("j_idt6:j_idt17:ref");
     
+    if(input_gara.style.display === 'block'){
+        input_gara.classList.remove("ui-state-disabled");
+        input_gara.style.display = "";
+    }else{
+        input_gara.classList.add("ui-state-disabled");
+        input_gara.style.display = "block";
+    }
+}
+
+function cort_clk(){
+    var input_cort = document.getElementById("j_idt6:j_idt17:aut");
     
- alert(element2.value);
-    for (var i = 0; i < 4; i++) {
-        console.log(sobget[i].value);
-        if(sobget[i].value === "asegurado"){
-           console.log(sobget[i].value+": 0"); 
-        }else{
-            
-        }
-       
-        
-       
-        
-       /* if(element.style.display === 'block' && sobget[i].value === "asegurado") {
-            element.classList.remove("ui-state-disabled");
-            element.classList.add("ui-state-default");
-            element.style.display = "";
-            alert("hecho");
-            break;
-        }else{
-            /*element.classList.remove("ui-state-default");
-            element.classList.add("ui-state-disabled");
-            element.style.display = "block";*/
-        // --}
-        
-        
-        /*else if(element.style.display === 'block' && sobget[i].value === "general"){
-            element.classList.remove("ui-state-default");
-            element.classList.add("ui-state-disabled");
-            element.style.display = "block";
-        }/*else if(){
-            element.classList.remove("ui-state-default");
-            element.classList.add("ui-state-disabled");
-            element.style.display = "block";
-        }*/
-    }                                                   
+    if(input_cort.style.display === 'block'){
+        input_cort.classList.remove("ui-state-disabled");
+        input_cort.style.display = "";
+    }else{
+        input_cort.classList.add("ui-state-disabled");
+        input_cort.style.display = "block";
+    }
 }
