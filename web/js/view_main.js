e22be6d13c5dc14f54525bf00a1d3bd2cbf30f8f
Yeltsin <Yeltsin@192.168.121.195>
2019-01-24 13:47:42 -0600

    Solucionado el modulo Tipo de atencion

--- web/js/view_main.js
+++ web/js/view_main.js
@@ -7,42 +7,15 @@ function aseg_clk(){
     if(combo_.style.display === 'block'){
         
         combo_.classList.remove("ui-state-disabled");
-        combo_.style.display = "";
-        
-        if(input_gara.style.display === 'block' && input_cort.style.display === 'block'){
-            alert("los dos estan en block");
-        }else if(input_gara.style.display === 'block' && input_cort.style.display === ''){
-            alert("garantia esta en block y cortesia no");
-        }else if(input_gara.style.display === '' && input_cort.style.display === 'block'){
-            alert("Cortesia esta en block y garantia no");
-        }else{
-            alert("los dos estan sin block");
-        }
-        
+        combo_.style.display = "";        
         input_gara.classList.add("ui-state-disabled");
         input_gara.style.display = "block";
-        //input_cort.classList.add("ui-state-disabled");
-        //input_cort.style.display = "block";
+        input_cort.classList.add("ui-state-disabled");
+        input_cort.style.display = "block";
         
-    }else{
-    //
-    
-    if(input_gara.style.display === 'block' && input_cort.style.display === 'block'){
-            alert("los dos estan en block");
-        }else if(input_gara.style.display === 'block' && input_cort.style.display === ''){
-            alert("garantia esta en block y cortesia no");
-        }else if(input_gara.style.display === '' && input_cort.style.display === 'block'){
-            alert("Cortesia esta en block y garantia no");
-        }else{
-            alert("los dos estan sin block");
-        }
-        
-        
-        
+    }else{    
         combo_.classList.add("ui-state-disabled");
         combo_.style.display = "block";
-        //input_gara.style.display = "";
-        //input_cort.style.display = "";
     }
 }
 
@@ -50,6 +23,20 @@ function gral_clk(){
     var combo_ = document.getElementById('j_idt6:j_idt17:asegu');
     var input_gara = document.getElementById("j_idt6:j_idt17:ref");
     var input_cort = document.getElementById("j_idt6:j_idt17:aut");
+    
+    
+    if(combo_.style.display === ''){
+        combo_.classList.add("ui-state-disabled");
+        combo_.style.display = "block";
+    }
+    if(input_gara.style.display === ''){
+        input_gara.classList.add("ui-state-disabled");
+        input_gara.style.display = "block";
+    }
+    if(input_cort.style.display === ''){
+        input_cort.classList.add("ui-state-disabled");
+        input_cort.style.display = "block";
+    }
 }
 
 function gara_clk(){
@@ -61,16 +48,13 @@ function gara_clk(){
         
         input_gara.classList.remove("ui-state-disabled");
         combo_.style.display = "block";
-        //input_cort.classList.add("ui-state-disabled");
-        //input_cort.style.display = "block";
+        input_cort.classList.add("ui-state-disabled");
+        input_cort.style.display = "block";
         combo_.classList.add("ui-state-disabled");
-        
         input_gara.style.display = "";
     }else{
         input_gara.classList.add("ui-state-disabled");
         input_gara.style.display = "block";
-        //input_cort.style.display = "";
-        //combo_.style.display = "";
     }
 }
 
@@ -89,7 +73,6 @@ function cort_clk(){
     }else{
         input_cort.classList.add("ui-state-disabled");
         input_cort.style.display = "block";
-        input_gara.style.display = "";
-        combo_.style.display = "";
+
     }
 }
